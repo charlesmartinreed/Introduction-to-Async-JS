10891a735e76439d8d2a4c73a83acbdd920eb668
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-02 15:24:17 -0600

    Create customer.json.

--- app.js
+++ app.js
@@ -1,52 +1,15 @@
 // EVENT LISTENER
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
 
-function loadData() {
-	// 1. Create an XHR Object
+function loadCustomer(e) {
 	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'customer.json', true);
 
-	// 2. Use the OPEN method - (typeOfRequest, file to make request to, async?)
-	xhr.open('GET', 'data.txt', true);
-
-	console.log('READYSTATE', xhr.readyState); //1
-
-	// OPTIONAL - used for spinners or loaders, while fetch occurs
-	xhr.onprogress = function(){
-		console.log('READYSTATE', xhr.readyState) //3
-	}
-
-	// 3. This occurs at object load
-	xhr.onload = function(){ //readyState step 4
-		//if status 200, everthing went well
-		// 200 = 'OK', 404 = 'Page not Found', 403: 'Forbidden', 500: Generic server error code indicating a server side failure
+	xhr.onload = function(){
 		if(this.status === 200) {
-				// this referring to the xhr object
-				//console.log(this.responseText);
-				document.querySelector('#output').innerHTML = `<p>${this.responseText}</p>`
+			console.log(this.responseText);
 		}
 	}
 
-	// ERROR CATCHING
-	xhr.onerror = function() {
-		console.log('Request error...');
-	}
-
-	// 4. Finalize
 	xhr.send();
-
-	// readyState values
-	// 0: request not initalized
-	// 1: server connection established
-	// 2: request received
-	// 3: processing request
-	// 4: request finished and response is ready
-
-	//before onload was introduced, state change would have to be checked
-	// xhr.onreadystatechange = function() {
-	// 	if(this.status === 200 && this.readyState === 4) {
-	// 		///
-	// 	}
-	// }
-
-
 }
